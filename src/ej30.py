50db0bfa9650b02ddf3390c0ffd547a884515835
amfrancisco <amfrancisco@iesrafaelalberti.es>
2024-10-01 18:03:40 +0200

    Corrección ejercicio 30

--- src/ej30.py
+++ src/ej30.py
@@ -3,18 +3,24 @@
 # El incremento y el total deben ser mayores que cero. Si no es así, el programa debe finalizar con un error o obligar a que introduzcan un valor correcto para ambos (os lo dejo a vuestra elección, la primera opción es más fácil, aunque el mundo está lleno de valientes).
 # Por ejemplo, si introducen los valores 1, 1 y 10, el programa mostrará en consola exactamente lo siguiente: SERIE => 1-2..3..4..5..6..7..8..9-10
 
-numero_inicial = int(input("Introduce el número inicial de la serie: "))
-numero_final = int(input("Introduce el número final de la serie: "))
-incremento = int(input("Introduce el valor del incremento: "))
+def serie(numero_inicial, numero_final, incremento):
 
-while numero_inicial <= 0 or incremento <= 0:
-    print("El incremento y el total deben ser mayores que cero.")
-    if numero_inicial == 0:
-        numero_final = int(input("Introduce un número final de la serie válido: "))
-    else:
-        incremento = int(input("Introduce un valor del incremento válido: "))
+    while numero_inicial <= 0 or incremento <= 0:
+        print("El incremento y el total deben ser mayores que cero.")
+        if numero_inicial == 0:
+            numero_final = int(input("Introduce un número final de la serie válido: "))
+        else:
+            incremento = int(input("Introduce un valor del incremento válido: "))
 
-serie = list(range(numero_inicial, numero_final + 1))
-serie = "-".join(map(str, serie))
+    serie = list(range(numero_inicial, numero_final + 1))
+    serie = "-".join(map(str, serie))
+    return f"{serie}"
 
-print(serie)
+def main():
+    numero_inicial = int(input("Introduce el número inicial de la serie: "))
+    numero_final = int(input("Introduce el número final de la serie: "))
+    incremento = int(input("Introduce el valor del incremento: "))
+    print(serie(numero_inicial, numero_final, incremento))
+
+if __name__ == "__main__":
+    main()
