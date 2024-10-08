d2a7c9ed09a11f575e65207469bdb9cf3d5f1eb7
amfrancisco <amfrancisco@iesrafaelalberti.es>
2024-10-08 17:44:08 +0200

    Pruebas Unitarias 2

--- tests/test_prueba1.py
+++ tests/test_prueba1.py
@@ -6,6 +6,23 @@ import os
 # Agregar el directorio raíz del proyecto (practica6) al sys.path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+# TEST comprobar_numero
+
+from src.prueba1 import comprobar_numero
+
+@pytest.mark.parametrize(
+    "input_x, input_y, expected",
+    [
+        ("1", "2", True),
+        ("3", "-5", True),
+        ("-4f", "patata", False)
+    ]
+)
+def test_comprobar_numero_params(input_x, input_y, expected):
+    assert comprobar_numero(input_x, input_y) == expected
+
+# TEST comparar_numero
+
 from src.prueba1 import comparar_numero
 
 @pytest.mark.parametrize(
